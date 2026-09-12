6e95351ed41f0467893055e553f9fbb43ba30a86
concorde bazimaziki <59648271+concordebazi@users.noreply.github.com>
2026-09-12 18:25:37 +0200

    Update map.js

--- business-map/map.js
+++ business-map/map.js
@@ -497,3 +497,26 @@ L.control.layers(
         collapsed: false
     }
 ).addTo(map);
+// ==========================================
+// DISTANCE & AREA MEASUREMENT
+// ==========================================
+
+const measureControl = new L.Control.Measure({
+
+    position: "topleft",
+
+    primaryLengthUnit: "meters",
+    secondaryLengthUnit: "kilometers",
+
+    primaryAreaUnit: "sqmeters",
+    secondaryAreaUnit: "hectares",
+
+    activeColor: "#20c7b7",
+    completedColor: "#146b5c",
+
+    localization: "sv"
+
+});
+
+
+measureControl.addTo(map);
